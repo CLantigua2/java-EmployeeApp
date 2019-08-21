8e1752ca5445b0c3afe7263c0128d643b0a0dbcd
Clantigua2 <clantigua954@gmail.com>
2019-08-21 12:21:38 -0400

    establishing employee getters and setters

--- EmployeeAPP/src/employeeApp/Employee.java
+++ EmployeeAPP/src/employeeApp/Employee.java
@@ -1,99 +1,38 @@
 package employeeApp;
 
-public class Employee
-{
-    private static int maxId = 0;
+public class Employee {
+    // fields - state - information
     private int id;
     private String fname;
     private String lname;
-    private double salary;
-    private Boolean has401K;
+    private double salary; // real numbers
+    private boolean has401K;
     private int companyId;
     private int healthPlanId;
 
-    public Employee(String fname, String lname, double salary, Boolean has401K, int companyId, int healthPlanId)
-    {
-        maxId++;
-        id = maxId;
+    // methods - behaviors
 
-        this.fname = fname;
-        this.lname = lname;
-        this.salary = salary;
-        this.has401K = has401K;
-        this.companyId = companyId;
-        this.healthPlanId = healthPlanId;
-    }
-
-    public int getId()
-    {
+    // getters (read fields)
+    public int getId() {
         return id;
     }
 
-    public String getName()
-    {
-        return fname + " " + lname;
+    public String getFname() {
+        return fname;
     }
 
-    public void setFname(String fname)
-    {
+    public String setFname(String fname) {
         this.fname = fname;
+        return "first name set";
     }
 
-    public void setLname(String lname)
-    {
+    public String setLname(String lname) {
         this.lname = lname;
+        return "last name set";
     }
 
-    public double getSalary()
-    {
-        return salary;
-    }
-
-    public void setSalary(double salary)
-    {
-        this.salary = salary;
-    }
-
-    public Boolean getHas401K()
-    {
-        return has401K;
-    }
-
-    public void setHas401K(Boolean has401K)
-    {
-        this.has401K = has401K;
-    }
-
-    public int getCompanyId()
-    {
-        return companyId;
-    }
-
-    public void setCompanyId(int companyId)
-    {
-        this.companyId = companyId;
-    }
-
-    public int getHealthPlanId()
-    {
-        return healthPlanId;
-    }
-
-    public void setHealthPlanId(int healthPlanId)
-    {
-        this.healthPlanId = healthPlanId;
-    }
-
-    @Override
-    public String toString()
-    {
-        String rtnStr = "id: " + id + "\n" +
-                        "fname: " + fname + "\n" +
-                        "lname: " + lname + "\n" +
-                        "salary: " + salary + "\n" +
-                        "has401K: " + has401K + "\n" +
-                        "companyId: " + companyId  + "\n" +
-                        "healthPlanId: " + healthPlanId + "\n";
-        return rtnStr;
+    // getter doesn't need the this keyword
+    public String getLname() {
+        return lname;
     }
 }
